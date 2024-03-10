402a0764dbcb6255ad68f07f9fd420b6b2eac408
CLong Lam <86845927+clonglam@users.noreply.github.com>
2024-03-10 03:38:40 -0700

    comment dahsboard layout

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,27 +1,27 @@
-import { SidebarNav } from "@/components/admin/SidebarNav";
-import { ScrollArea } from "@/components/ui/scrollArea";
-import { dashboardConfig } from "@/config/dashboard";
-import createServerClient from "@/lib/supabase/server";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import { SidebarNav } from "@/components/admin/SidebarNav"
+import { ScrollArea } from "@/components/ui/scrollArea"
+import { dashboardConfig } from "@/config/dashboard"
+import createServerClient from "@/lib/supabase/server"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
 
 interface DashboardLayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export default async function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
-  const cookieStore = cookies();
-  const supabase = createServerClient({ cookieStore });
+  // const cookieStore = cookies();
+  // const supabase = createServerClient({ cookieStore });
 
-  const {
-    data: { user },
-    error: authError,
-  } = await supabase.auth.getUser();
-  if (authError || !user) {
-    redirect("/sign-in");
-  }
+  // const {
+  //   data: { user },
+  //   error: authError,
+  // } = await supabase.auth.getUser()
+  // if (authError || !user) {
+  //   redirect("/sign-in")
+  // }
 
   return (
     <div className="mx-auto px-[3rem] max-w-[2500px] pt-[50px] flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10 bg-white">
@@ -34,5 +34,5 @@ export default async function DashboardLayout({
         {children}
       </main>
     </div>
-  );
+  )
 }
